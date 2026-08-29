10883aa405425670ddccec39aa098fd113c15841
Hussain-jutt3012 <hali171825@gmail.com>
2026-08-30 00:38:21 +0500

    Update production CORS origin

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ app.use(
         origin: [
             "http://localhost:5173",
             "https://my-e02z7m63o-my-team-af88.vercel.app",
-            "https://my-lms-pearl.vercel.app"
+            "https://my-lms-pearl.vercel.app",
+            "https://my-lms-git-main-my-team-af88.vercel.appgit"
         ],
     })
 );
